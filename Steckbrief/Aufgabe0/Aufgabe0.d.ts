7bd73dc29e02cae5acc658059a3a43b5d5d40651
stefa <stefa@LAPTOP-I6O9RBF4>
2018-10-07 12:15:50 +0200

    aufgabe0

---
+++ Steckbrief/Aufgabe0/Aufgabe0.d.ts
@@ -0,0 +1,2 @@
+declare var i: string;
+declare function main(): void;
--- Steckbrief/Aufgabe0/Aufgabe0.ts
+++ Steckbrief/Aufgabe0/Aufgabe0.ts
@@ -1,19 +1,18 @@
 /* Aufgabe: Aufgabe 0
 Name: Stefan Merdian
-Matrikel: 
 Datum: 05.10.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
 Er wurde nicht kopiert und auch nicht diktiert. */
-var aufgabe0;
-(function (aufgabe0) {
-    var i = "";
+
+
+
+    var i: string = "";
     function main() {
         var i = prompt("Dein Name ?");
         var node = document.getElementById("content");
-        node.innerHTML += "Hallo ";
+        node.innerHTML += "Servus ";
         node.innerHTML += i;
-        node.innerHTML += ", freut mich, dass du hier bist.";
-        console.log("Hallo ", i, ", freut mich, dass du hier bist.");
+        node.innerHTML += ", wie gehts dir?.";
+        console.log("Hallo ", i, ", wie gehts dir?.");
     }
     document.addEventListener('DOMContentLoaded', main);
-})(aufgabe0 || (aufgabe0 = {}));
